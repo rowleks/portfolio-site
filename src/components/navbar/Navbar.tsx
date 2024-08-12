a762f9000596b00c581daf5e3d5c8c3cb1394059
rowleks <momohrowland@gmail.com>
2024-08-12 09:29:57 +0100

    fixed hamburger-nav for mobile

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,17 +1,30 @@
 import { NavLink } from "react-router-dom";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import "./navbar.scss";
+import { useState } from "react";
 function Navbar() {
+
+  const [open, setOpen] = useState(false)
+  const toggleNav = () => setOpen(!open)
   return (
     <div className="navbar">
-        <NavLink className="nav" to="/"><h2>Logo</h2></NavLink>
-        <img src="/hamburger.png" alt="" />
+        <NavLink onClick={()=> setOpen(false)} className={open ? "ham-logo" : "nav-logo"} to="/"><h2>Logo</h2></NavLink>
+        <img className="nav-icon" src="/hamburger.png" alt="" onClick={toggleNav} />
         <div className="right-hand">
           <NavLink className={({ isActive }) => (isActive ? 'active' : '')} to="/about">About</NavLink>
           <AnchorLink href="#projects">Projects</AnchorLink>
           <AnchorLink href="#contact">Contact</AnchorLink>
 
           
+        </div>
+
+        <div className={`ham-nav ${open ? "open": ""}`}>
+          <div className="ham-nav-links">
+            <NavLink onClick={()=> setOpen(false)} className={({ isActive }) => (isActive ? 'active' : '')} to="/about">About</NavLink>
+            <AnchorLink onClick={()=> setOpen(false)} href="#projects">Projects</AnchorLink>
+            <AnchorLink onClick={()=> setOpen(false)} href="#contact">Contact</AnchorLink>
+          </div>
+
         </div>
 
       
