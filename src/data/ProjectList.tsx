2a1219f02e77d675096bac1729081a09d0e5636d
rowleks <momohrowland@gmail.com>
2025-01-04 17:56:53 +0100

    added projects

--- src/data/ProjectList.tsx
+++ src/data/ProjectList.tsx
@@ -3,10 +3,14 @@ export const myProjects = [
     
     { id: 2, imgSrc: "./illst-portfolio.png", title: "Illustrated Portfolio Website", description: "An illustrated portfolio website project built using React and TypeScript", sourceCode: "https://github.com/rowleks/illustrated-portfolio", live: "https://illustrated-portfolio.netlify.app/" },
 
-    { id: 3, imgSrc: "./crush-pizza.png", title: "Crush Pizza", description: "A mobile only restaurnt website for ordering pizzas and other snacks", sourceCode: "https://github.com/rowleks/crush-pizza", live:"http://crush-pizza.netlify.app/" },
+    { id: 3, imgSrc: "./crush-pizza.png", title: "Crush Pizza", description: "A mobile only restaurnt website for ordering pizzas and other snacks built with React", sourceCode: "https://github.com/rowleks/crush-pizza", live:"http://crush-pizza.netlify.app/" },
 
 
-    { id: 4, imgSrc: "./rps-game.png", title: "Rock, Paper, Scissors", description: "A website for playing the classical rock, paper, scissors game built with HTML, CSS and JavaScript", sourceCode: "https://github.com/rowleks/rowleks.github.io/tree/main/rock-paper-scissors", live:"https://rowleks.github.io/rock-paper-scissors/" },
+    { id: 4, imgSrc: "./rps-game.png", title: "Rock, Paper, Scissors", description: "A website for playing the classical rock, paper, scissors game built with HTML, CSS and JS", sourceCode: "https://github.com/rowleks/rowleks.github.io/tree/main/rock-paper-scissors", live:"https://rowleks.github.io/rock-paper-scissors/" },
+
+    { id: 5, imgSrc: "./tip-calc.png", title: "Tip Calculator App", description: "Solution to a frontend mentor challenge built with HTML, CSS and JS", sourceCode: "https://github.com/rowleks/fe-tip-calculator", live:"https://fe-tip-calculator.netlify.app/" },
+
+    { id: 6, imgSrc: "./easybank.jpg", title: "Easybank Landing Page", description: "Solution to a frontend mentor challenge built with HTML, CSS and JS", sourceCode: "https://github.com/rowleks/easybank", live:"https://fe-easybank.netlify.app/" },
 
     // { id: 5, imgSrc: "./project5.png", title: "Whole Lotta Love", description: "Lorem ipsum dolor" },
   ]
